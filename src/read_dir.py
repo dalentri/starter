8cea38858c5aed6e901e82c3a9a1b54ae0446fa1
dalentri <aydenjmason05@gmail.com>
2026-03-15 16:31:29 -0500

    Created a list for the music files to populate to for the tui in read_dir

--- src/read_dir.py
+++ src/read_dir.py
@@ -10,14 +10,23 @@ class ReadDir:
 
     # Scans the user's folder for files / directories
     def scan_folder(self):
-
         music_folder = Path(self.path)
+        # Inits a list of the music that will be found later
+        music_list = []
 
         # Goes through the folder and lists all files
         # TODO: Make this iterable in tui
         for item in music_folder.iterdir():
-            if not item.name.endswith(".m4a") and not item.name.startswith("."):
+            if (
+                not item.name.endswith(".m4a")
+                and not item.name.startswith(".")
+                and item.is_file()
+            ):
                 print(item.name)
+                print(item)
+
+                # Append the a tuple containing both the item name and the path to the item to the list
+                music_list.append((item.name, str(item)))
 
     def get_full_path(self, filename: str):
         full_path = Path(self.path) / filename
