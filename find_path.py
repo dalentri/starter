c5e17b485ab70aa6907898880b4d96afa30d555d
dalentri <aydenjmason05@gmail.com>
2026-03-14 19:27:39 -0500

    Refactored find_platform function to dynamically find the path of the music folder-

--- find_path.py
+++ find_path.py
@@ -1,21 +1,25 @@
 import platform
+from pathlib import Path
 
 
 class FindPath:
     # Finds the user's platform
-    def find_platform(self) -> tuple[str, str]:
-        user_platform = platform.system()
-        print(f"Platform: {user_platform}")
+    def find_platform(self) -> tuple:
 
+        user_platform = platform.system()
+        user_path = Path.home() / "Music"
+
+        print(f"Platform: {user_platform}")
+        print(f"Path: {user_path}")
         # We return both the platform and the path to later pass into ReadDir
         # Macos
         if user_platform == "Darwin":
-            return "Darwin", "/Users/<User>/Music"
+            return "Darwin", user_path
         # Win
         elif user_platform == "Windows":
-            return "Windows", "C:\\Users\\<User>\\Music"
+            return "Windows", user_path
         # Linux
         elif user_platform == "Linux":
-            return "Linux", "/home/<User>/Music"
+            return "Linux", user_path
         else:
             return "Platform not supported.", "Path not found."
