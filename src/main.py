81d679163def08c0340e7ae588960a8d762563e3
dalentri <aydenjmason05@gmail.com>
2026-03-16 10:03:36 -0500

    remove redundant code in main.py func since the tui will take care of it

--- src/main.py
+++ src/main.py
@@ -1,24 +1,9 @@
 # Import the files from the same directory
-from . import find_path, read_dir, music_controls, tui
 from src.tui import TUI
 
 
 def main():
-    # Init find folder class
-    find_folder = find_path.FindPath()
-
-    platform, path = find_folder.find_platform()
-
-    # Init scan directory class and pass in prev platform and path variables
-    scan_directory = read_dir.ReadDir(platform, path)
-
-    scan_directory.scan_folder()
-
-    control_music = music_controls.MusicControls(
-        scan_directory.get_full_path("KlassikBlendz.wav")
-    )
-    control_music.load_song()
-    control_music.play_song()
+    pass
 
 
 app = TUI()
